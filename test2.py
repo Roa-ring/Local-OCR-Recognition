f9fcb878806a606ab29c73b8fcc70d4aafc8442e
Xuyuecheng <1779499859@qq.com>
2026-07-07 15:46:28 +0800

    添加了opencv-python  库，完成了基本逻辑

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 from paddleocr import PaddleOCR
 
-input = "13d0a418-fe7b-4d9b-873e-f2bbccb2554c.png"
+input = "c2990ee0d065aa5b079f5d55641e47ec.jpg"
 ocr = PaddleOCR(
     text_detection_model_name="PP-OCRv6_medium_det",
     text_recognition_model_name="PP-OCRv6_medium_rec",
@@ -8,7 +8,7 @@ ocr = PaddleOCR(
     use_doc_unwarping=False,
     use_textline_orientation=True,
 )
-result = ocr.predict(input)
+result = ocr.predict("c2990ee0d065aa5b079f5d55641e47ec.jpg")
 for res in result:
     res.print()
     res.save_to_img("output")
