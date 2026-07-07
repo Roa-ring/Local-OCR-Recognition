98ab64c8b836b8bc882267445686eeb14d41aeae
LuckyE137 <2215150956@qq.com>
2026-07-07 16:17:36 +0800

    Refactor image processing to use input directory
    添加了glob依赖，现在可以根据路径批量处理路径内的png和jpg格式文件

--- test2.py
+++ test2.py
@@ -1,10 +1,5 @@
 from paddleocr import PaddleOCR
-<<<<<<< HEAD
-
-input = "c2990ee0d065aa5b079f5d55641e47ec.jpg"
-=======
 import glob
->>>>>>> 72871ef02214ed24c0603b3864d56657a6597ad6
 ocr = PaddleOCR(
     text_detection_model_name="PP-OCRv6_medium_det",
     text_recognition_model_name="PP-OCRv6_medium_rec",
@@ -12,13 +7,6 @@ ocr = PaddleOCR(
     use_doc_unwarping=False,
     use_textline_orientation=True,
 )
-<<<<<<< HEAD
-result = ocr.predict("c2990ee0d065aa5b079f5d55641e47ec.jpg")
-for res in result:
-    res.print()
-    res.save_to_img("output")
-    res.save_to_json("output")
-=======
 way=input()
 image_list = glob.glob(rf"{way}/*.png") + glob.glob(rf"{way}/*.jpg")
 for img_path in image_list:
@@ -28,4 +16,3 @@ for img_path in image_list:
         res.print()
         res.save_to_img("output")
         res.save_to_json("output")
->>>>>>> 72871ef02214ed24c0603b3864d56657a6597ad6
